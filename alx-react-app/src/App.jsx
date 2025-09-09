1c4b3efaf40b4ff84e41689ea25d7dff990b225a
Susanadjanie1 <susanadjanie1@gmail.com>
2025-09-09 22:00:11 +0000

    Hearder, MainContent, Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
 
   return (
     <>
       <WelcomeMessage />
+      <Header />  
+      <MainContent />
+      <Footer />
     </>
   )
 }
