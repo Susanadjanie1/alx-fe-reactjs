08f5fa70ea9bc61fb5931ecb006d03428b989c58
Susanadjanie1 <susanadjanie1@gmail.com>
2025-09-14 04:12:38 +0000

    alx-react-app-new

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: 'white/50', color: 'black', textAlign: 'center' }}>
+  <h1>My Favorite Cities</h1>
+</header>
+  )
+}
+
+export default Header
