2aef97db7dbbccb4a10a41ee6f948913386596e5
Susanadjanie1 <susanadjanie1@gmail.com>
2025-09-14 04:27:42 +0000

    span added to styling

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-// Pass `props` as an argument to the function.
-// Using destructuring is a common and clean way to do this.
 const UserProfile = ({ name, age, bio }) => {
   return (
     <div style={{ border: '2px solid blue', padding: '12px', margin: '12px' }}>
       <h2 style={{ color: 'blue' }}>{name}</h2>
-      <p>Age: {age}</p>
+      <p>
+        Age: <span style={{ fontSize: '10px' }}>{age}</span>
+      </p>
       <p>Bio: {bio}</p>
     </div>
   );
