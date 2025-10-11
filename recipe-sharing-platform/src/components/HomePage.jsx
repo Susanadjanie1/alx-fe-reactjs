6434090cfbe352dc9388886b45bd0edd14e905be
Susanadjanie1 <susanadjanie1@gmail.com>
2025-10-11 18:45:26 +0000

    home page update

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -27,8 +27,8 @@ const HomePage = () => {
                 Explore Delicious Ghanaian Recipes 🍴
             </h1>
 
-            {/* Responsive Grid Layout (Step 4) */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+            
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-8">
                 
                 {recipes.map((recipe) => (
                     <div
