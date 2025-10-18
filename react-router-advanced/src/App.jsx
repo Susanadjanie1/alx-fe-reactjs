809db2f708b5b9dec090555aa40d845846f1f66a
Susanadjanie1 <susanadjanie1@gmail.com>
2025-10-18 17:17:47 +0000

    App.jsx updated

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,54 +1,34 @@
-import React, { useState } from 'react';
-import { Routes, Route, Link, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Profile from './pages/Profile';
-import ProfileDetails from './pages/ProfileDetails';
-import ProfileSettings from './pages/ProfileSettings';
-import BlogPost from './pages/BlogPost';
-import ProtectedRoute from './components/ProtectedRoute';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Blog from "./components/Blog";
+import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const toggleAuth = () => {
-    setIsAuthenticated((prev) => !prev);
-  };
-
+function App() {
   return (
-    <div>
-      <nav style={{ display: 'flex', gap: '20px', marginBottom: '20px' }}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/posts/1">Example Post</Link>
-        <button onClick={toggleAuth}>
-          {isAuthenticated ? 'Logout' : 'Login'}
-        </button>
-      </nav>
-
+    <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-
-       
         <Route
           path="/profile/*"
           element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
+            <ProtectedRoute>
               <Profile />
             </ProtectedRoute>
           }
         />
-
-     
-        <Route path="/posts/:postId" element={<BlogPost />} />
-
-        {/* Redirect unknown routes */}
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
+
+
